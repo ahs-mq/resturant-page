e5036eac107f3aa775afdefbb3aa105faabb0ef7
ahs-mq <141071955+ahs-mq@users.noreply.github.com>
2025-05-12 16:46:17 +0400

    completed project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
+//imported js contains array to be used for creating HTML elements
+
 import "./styles.css";
+import {menu , location , about} from "./mods.js";
+const content = document.getElementById("body");
+const menuBtn = document.getElementById("menu");
+const locationBtn = document.getElementById("location");
+const aboutBtn = document.getElementById("about");
+
+class DOMCreator {
+    constructor(elements) {
+        this.elements = elements;
+    }
+
+    createElements() {
+        const container = document.createElement("div");
+        container.classList.add("card");
+        this.elements.forEach(element => {
+            const el = document.createElement(element.type);
+            //used if statments in case future divs are missing a classname or desc.
+            if (element.content){
+                const head = document.createElement("h3");
+                head.textContent = element.content;
+                head.classList.add("title");
+                el.appendChild(head);
+
+            }
+            if (element.className) el.classList.add(element.className);
+            
+            if (element.description) {
+                const desc = document.createElement("p");
+                desc.textContent = element.description;
+                desc.classList.add("description");
+                el.appendChild(desc);
+            }
+
+            container.appendChild(el);
+        });
+        return container;
+    }
+}
+
+menuBtn.addEventListener("click",()=>{
+    const menuInfo = new DOMCreator(menu);
+    content.innerHTML = '';
+    content.appendChild(menuInfo.createElements());
+});
+
+locationBtn.addEventListener("click", ()=>{
+    const locationInfo = new DOMCreator(location);
+    content.innerHTML = '';
+    content.appendChild(locationInfo.createElements());
+});
+
+aboutBtn.addEventListener("click", ()=>{
+    const aboutInfo = new DOMCreator(about);
+    content.innerHTML = '';
+    content.appendChild(aboutInfo.createElements());
+});
---
+++ src/mods.js
@@ -0,0 +1,14 @@
+export const menu = [
+    { type: "div", content: "Creamy Pasta", description: "A delicious pasta with creamy sauce.", className: "menu-item" },
+    { type: "div", content: "Grilled Salmon", description: "Grilled salmon with fresh vegetables.", className: "menu-item" },
+    { type: "div", content: "Steak", description: "Dry aged Ribeye with a side of your choice.", className: "menu-item" }
+];
+
+export const location = [
+    { type: "div", content: "Location", description: "121 Funny St Blah State 13134", className:"where"},
+    { type: "div", content: "Timing", description: "Mon-Fri 10 AM ~ 10 PM", className:"where"}
+];
+
+export const about = [
+    { type: "div", content: "Who We are", description: "Est. 1000 BC. We make the Finest food on Earth", className:"who"}
+];
